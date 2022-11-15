3f77c22a045fc6b9bdc7542415ce56a2f87cf95c
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2022-11-15 23:59:13 +0700

    Push semua

---
+++ src/tandain.py
@@ -0,0 +1,8 @@
+# console       : 
+# log           : untuk print
+# prompt        : untuk input
+# var           : deklarasi var bisa diubah
+# const         : deklarasi var tidak bisa diubah dan harus di define
+# parseInt      : untuk mengubah string ke int
+# let           : kamus lokal
+
---
+++ src/uji.js
@@ -0,0 +1,12 @@
+
+
+console.log('Hello World\n');
+let a
+a = 4**2
+console.log(a)
+/*  */
+// /*  */
+
+if (a > 4){
+    //BODY
+} 
