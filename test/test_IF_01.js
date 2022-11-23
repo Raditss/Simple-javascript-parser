53c656ee8fc00ef9e0e1a2212d8c0af12eaa6b5b
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2022-11-23 15:58:04 +0700

    Add: test case for conditional (if else)

---
+++ test/test_IF_01.js
@@ -0,0 +1,12 @@
+A = 4
+B = 5
+/* Disini terdapat uji coba if, else if, dan else */
+
+
+if (A === B) {
+    /* Body bisa apa saja */
+} else if (A == B){
+    /* Body bisa apa saja */
+} else {
+    /* Body bisa apa saja*/
+}
