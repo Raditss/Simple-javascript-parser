a63c57a314066a6bc13c4624f0a7239f9bfc8160
bewe <16521486@mahasiswa.itb.ac.id>
2022-11-25 01:02:45 +0700

    update

--- src/test_IF_01.js
+++ src/test_IF_01.js
@@ -1,1 +1,13 @@
-continue
+function do_something(x) {
+    // This is a sample comment
+    if (x == 0) {
+    } else if (x == 1) {
+      if (true) {
+      } else {
+      }
+    } else if (x == 32) {
+    } else {
+      console.log("Momen");
+    }
+  }
+  
