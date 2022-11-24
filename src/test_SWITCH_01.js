dcaa878ab03716241a6de15bf01a6157ed24cf43
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2022-11-24 20:44:56 +0700

    Add: switch test case

---
+++ src/test_SWITCH_01.js
@@ -0,0 +1,18 @@
+switch (X){
+    case 1:
+    case __init__:;;;;;;
+
+    case 3:
+        saya 
+
+    case noYou:
+    
+    default: suhu 
+
+    case 7: 
+        SVGFEGaussianBlurElement
+        saya
+
+
+
+}
