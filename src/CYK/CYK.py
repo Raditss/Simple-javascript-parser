a4562bd385511e43a154b72f3c1b9e84da4d0264
Raditss <raditya.naufal.a@gmail.com>
2022-11-25 21:27:38 +0700

    ?

--- src/CYK/CYK.py
+++ src/CYK/CYK.py
@@ -1,16 +1,16 @@
-def cykParse(w, R):
-    n = len(w)
+def cykParse(tkn, CNF):
+    n = len(tkn)
     T = [[set([]) for j in range(n)] for i in range(n)]
     for j in range(0, n):
-      for left, rule in R.items():
+      for left, rule in CNF.items():
         for right in rule:    
-          if len(right) == 1 and right[0] == w[j]:
+          if len(right) == 1 and right[0] == tkn[j]:
             T[j][j].add(left)
 
       
       for i in range(j, -1, -1):     
         for k in range(i, j):  
-          for left, rule in R.items():
+          for left, rule in CNF.items():
             for right in rule:
               if len(right) == 2 and right[0] in T[i][k] and right[1] in T[k + 1][j]:
                 T[i][j].add(left)
